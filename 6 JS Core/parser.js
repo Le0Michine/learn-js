de5b31dfa7f7d30a01a2245b7b63d5cde28be7f7
Leo <leomichine@gmail.com>
2017-01-23 17:32:42 +0100

    small fixes

--- 6 JS Core/parser.js
+++ 6 JS Core/parser.js
@@ -1,5 +1,5 @@
-// var operators = ["*", "/", "+", "-"];
-var operators = [
+// let operators = ["*", "/", "+", "-"];
+const operators = [
   { operator: '*', method: (a, b) => a * b, escaped: '\\*' },
   { operator: '/', method: (a, b) => a / b, escaped: '\\/' },
   { operator: '+', method: (a, b) => a + b, escaped: '\\+' },
@@ -7,29 +7,27 @@ var operators = [
 ];
 
 const parser = function(input, extraOperators) {
-  if (extraOperators) {
-    operators = extraOperators.concat(operators);
-  }
-  var operatorSigns = operators.map(x => x.operator);
-  var operatorsRegex = '(' + operators.map(x => x.escaped || x.operator).join(')|(') + ')';
-  var operatorsAndNumbers = new RegExp('^([\\d ]|' + operatorsRegex + ')*$');
+  const allOperators = extraOperators ? extraOperators.concat(operators) : operators;
+  const operatorSigns = allOperators.map(x => x.operator);
+  const operatorsRegex = '(' + allOperators.map(x => x.escaped || x.operator).join(')|(') + ')';
+  const operatorsAndNumbers = new RegExp('^([\\d ]|' + operatorsRegex + ')*$');
   if (!input || !input.trim()) {
     throw new Error('input string is empty or undefined');
   }
   if (!operatorsAndNumbers.test(input)) {
     throw new Error('input has incorrect format');
   }
-  var matches = input.match(new RegExp('(-?\\d+)|' + operatorsRegex, 'g'));
-  var stack = [];
-  for (var match of matches) {
-    var i = operatorSigns.indexOf(match);
+  const matches = input.match(new RegExp('(-?\\d+)|' + operatorsRegex, 'g'));
+  const stack = [];
+  for (let match of matches) {
+    const i = operatorSigns.indexOf(match);
     if (i > -1) {
       if (stack.length < 2) {
         throw Error('input has incorrect format');
       } else {
-        var op2 = stack.pop();
-        var op1 = stack.pop();
-        stack.push(operators[i].method(op1, op2));
+        const op2 = stack.pop();
+        const op1 = stack.pop();
+        stack.push(allOperators[i].method(op1, op2));
       }
     } else if (!isNaN(match)) {
       stack.push(+match);
--- 6 JS Core/parser.spec.js
+++ 6 JS Core/parser.spec.js
@@ -1,9 +1,9 @@
-var assert = require('assert');
-var parser = require('./parser');
+let assert = require('assert');
+let parser = require('./parser');
 
 describe('parser', () => {
   describe('happy day', () => {
-    var testData = [
+    let testData = [
       { input: "5", result: "5"},
       { input: "5 4 *", result: "20"},
       { input: "52 14 *", result: "728"},
@@ -17,7 +17,7 @@ describe('parser', () => {
       it ('should return ' + testCase.result + ' for input "' + testCase.input + '"', () => {
         // arrange
         // act
-        var result = parser(testCase.input);
+        let result = parser(testCase.input);
 
         // assert
         assert.equal(result, testCase.result);
@@ -26,7 +26,7 @@ describe('parser', () => {
   });
 
   describe('negative', () => {
-    var testData = [
+    let testData = [
       { input: "-" },
       { input: "5 -" },
       { input: "5 6 - +" },
@@ -47,7 +47,7 @@ describe('parser', () => {
   });
 
   describe('extra operators', () => {
-    var extraOperators = [
+    let extraOperators = [
       { operator: '%', method: (a, b) => a % b },
       { operator: 'avg', method: (a, b) => (a + b) / 2 }
     ];
@@ -62,7 +62,7 @@ describe('parser', () => {
     it('should return 3 for input "1 2 3 % +"', () => {
       // arrange
       // act
-      var result = parser("1 2 3 % +", extraOperators);
+      let result = parser("1 2 3 % +", extraOperators);
 
       // assert
       assert.equal(result, '3');
@@ -71,7 +71,7 @@ describe('parser', () => {
     it('should return 3 for input "2 7 avg"', () => {
       // arrange
       // act
-      var result = parser("2 7 avg", extraOperators);
+      let result = parser("2 7 avg", extraOperators);
 
       // assert
       assert.equal(result, '4.5');
