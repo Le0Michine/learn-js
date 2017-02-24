e9d7610a2ce6d86b0700ed59b689114785672c1e
Le0Michine <leomichine@gmail.com>
2017-02-24 11:31:04 +0100

    refactor

--- 8 DOM SPA History API/src/app.js
+++ 8 DOM SPA History API/src/app.js
@@ -4,28 +4,27 @@ const ROUTES = [
     { url: '/sign-up', templateUrl: '/src/templates/sign-up.html' }
 ];
 
-class Router {
-  constructor(routes, basePath, onNavigation) {
-    console.log('init router', location.pathname);
+(function () {
+  class Router {
+    constructor(routes, basePath, onNavigation) {
+      console.log('init router', location.pathname, basePath);
 
-    this.routes = routes || [];
-    this.basePath = basePath ? basePath.replace(/\/$/,'') : basePath;
-    this.currentRoute = this.toShortPath(location.pathname);
+      this.routes = routes || [];
+      this.basePath = basePath ? basePath.replace(/\/$/,'') : basePath;
+      this.currentRoute = this.toShortPath(location.pathname);
+      this.onNavigation = onNavigation || (() => void 0);
 
-    if (!this.routes.find(r => r.url === this.currentRoute)) {
-      throw new Error(`Unknown path ${this.currentRoute}`);
+      if (!this.routes.find(r => r.url === this.currentRoute)) {
+        throw new Error(`Unknown path ${this.currentRoute}`);
+      }
+
+      this.listenLinks()
+      this.listenNavigationEvents();
+
+      this.onNavigation(this.toFullPath(this.routes.find(r => r.url === this.currentRoute).templateUrl));
     }
 
-    document.querySelectorAll('a[route]')
-      .forEach(link => {
-        link.href = link.href.replace(location.origin, this.basePath || '');
-        link.addEventListener('click', (event) => {
-          event.preventDefault();
-          history.pushState({ }, '', link.href);
-          this.dispatchNavigationEvent();
-        });
-      });
-
+    listenNavigationEvents() {
       document.addEventListener('navigate', () => {
         const path = location.pathname;
         const route = this.routes.find(r => r.url === this.toShortPath(path));
@@ -35,51 +34,65 @@ class Router {
         } else {
           console.log('navigate to', path);
           this.currentRoute = this.toShortPath(path);
-          onNavigation(this.toFullPath(route.templateUrl));
+          this.onNavigation(this.toFullPath(route.templateUrl));
         }
       });
 
       window.addEventListener('popstate', () => {
         this.dispatchNavigationEvent();
       });
+    }
 
-      onNavigation(this.toFullPath(this.routes.find(r => r.url === this.currentRoute).templateUrl));
-  }
-
-  toShortPath(path) {
-    return path.replace(this.basePath, '');
-  }
-
-  toFullPath(path) {
-    return (this.basePath || '') + path;
-  }
-
-  dispatchNavigationEvent() {
-    document.dispatchEvent(new Event('navigate'));
-  }
-}
-
-
-(function wd(rootName) {
-  const root = document.querySelector(`[wd-root=${rootName}]`);
-
-  if (!root) {
-    throw new Error(`Unable to find root element with name ${rootName}`);
-  } else {
-    const router = new Router(ROUTES, location.pathname, (templateUrl) => {
-      fetch(templateUrl).then(x => {
-        const reader = x.body.getReader();
-        let template = '';
-        const bufferProcessor = (result) => {
-          template += String.fromCharCode.apply(null, result.value);
-          if (!result.done) {
-            reader.read().then(bufferProcessor);
-          } else {
-            root.innerHTML = template;
-          }
-        };
-        reader.read().then(bufferProcessor);
+    listenLinks() {
+      document.addEventListener('click', (event) => {
+        if (event.target.hasAttribute('route')) {
+          event.preventDefault();
+          history.pushState({ }, '', event.target.href);
+          this.dispatchNavigationEvent();
+        }
       });
-    });
+    }
+
+    toShortPath(path) {
+      return path.replace(this.basePath, '');
+    }
+
+    toFullPath(path) {
+      return (this.basePath || '') + path;
+    }
+
+    dispatchNavigationEvent() {
+      document.dispatchEvent(new Event('navigate'));
+    }
   }
-})("app");
+
+  class WD {
+    constructor() {}
+
+    static createRoot(rootName) {
+      const root = document.querySelector(`[wd-root=${rootName}]`);
+      if (!root) {
+        throw new Error(`Unable to find root element with name ${rootName}`);
+      } else {
+        const router = new Router(ROUTES, location.pathname, (templateUrl) => {
+          fetch(templateUrl).then(x => {
+            const reader = x.body.getReader();
+            let template = '';
+            const bufferProcessor = (result) => {
+              template += String.fromCharCode.apply(null, result.value);
+              if (!result.done) {
+                reader.read().then(bufferProcessor);
+              } else {
+                root.innerHTML = template;
+              }
+            };
+            reader.read().then(bufferProcessor);
+          });
+        });
+      }
+    }
+  }
+
+  const wd = new WD();
+  WD.createRoot('app');
+})();
